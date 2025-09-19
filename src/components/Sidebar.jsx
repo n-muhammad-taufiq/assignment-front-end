904b9b9c8e01c99c1c8b96c27e8edf58fcedc7cc
n-muhammad-taufiq <n.muhammad.taufiq@outlook.com>
2025-09-19 17:44:36 +0530

    feat:build layout for sidebar

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <div className='flex flex-col fixed left-0 top-0 bottom-0 border-r border-r-gray-100 px-2 pt-5  z-20'>
+
+    </div>
+  )
+}
+
+export default Sidebar

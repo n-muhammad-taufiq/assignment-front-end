8a37e5a8475df3674213ca64d63db2529f5f2bf5
n-muhammad-taufiq <n.muhammad.taufiq@outlook.com>
2025-09-19 17:49:11 +0530

    feat:implement sidebar header with logo and title

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 const Sidebar = () => {
   return (
     <div className='flex flex-col fixed left-0 top-0 bottom-0 border-r border-r-gray-100 px-2 pt-5  z-20'>
-
+      <div className='flex gap-x-1 items-center pb-2'>
+            <img className='h-7' src="./src/assets/icons/logo.png" alt="" />
+            <h1 className='font-black text-base'>Tectra <span className='text-gray-950 font-bold'>Clinic</span></h1>
+        </div>
+      <span className='h-[1px] w-full bg-gray-200 self-center'></span>
     </div>
   )
 }

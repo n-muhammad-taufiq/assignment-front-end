6b76fbba44234ccde849bce4ad5d3280f6a6938f
n-muhammad-taufiq <n.muhammad.taufiq@outlook.com>
2025-09-20 11:15:16 +0530

    feat:implement header

---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <div className='flex flex-col p-5 bg-gray-50 w-full h-screen'>
+        <h1 className='text-lg font-bold'>Dashboard <span className='text-slate-300'>/ Analytics and Reports</span></h1>
+    </div>
+  )
+}
+
+export default Dashboard

0618c34bdaaf28c4f3b388ffde7c2f29127f01bd
n-muhammad-taufiq <n.muhammad.taufiq@outlook.com>
2025-09-21 10:19:00 +0530

    feat:implement StatisticCard

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import StatisticCard from './StatisticCard'
 
 const Dashboard = () => {
   return (
-    <div className='flex flex-col p-5 bg-gray-50 w-full h-screen'>
+    <div className='flex flex-col gap-y-5 p-5 bg-gray-50 w-full h-full'>
         <h1 className='text-lg font-bold'>Dashboard <span className='text-slate-300'>/ Analytics and Reports</span></h1>
+        <div className='flex gap-x-4'>
+        <StatisticCard title={'Total Visitors'} previousValue={100} currentValue={136} total={42946}></StatisticCard>
+        <StatisticCard title={'Paid Visitors'} previousValue={100} currentValue={126} total={7929}></StatisticCard>
+        <StatisticCard title={'Total Appointments'} previousValue={100} currentValue={126} total={4199}></StatisticCard>
+        <StatisticCard title={'New Patients'} previousValue={100} currentValue={126} total={1647}></StatisticCard>
+        </div>
     </div>
   )
 }

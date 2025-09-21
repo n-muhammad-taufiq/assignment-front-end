3df196629f4569186695ebd698779514685b8b8a
n-muhammad-taufiq <n.muhammad.taufiq@outlook.com>
2025-09-21 18:17:06 +0530

    feat:implement Expenses Component

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,15 +1,37 @@
 import React from 'react'
 import StatisticCard from './StatisticCard'
+import Cashflow from './Cashflow'
+import Expenses from './Expenses'
 
 const Dashboard = () => {
   return (
-    <div className='flex flex-col gap-y-5 p-5 bg-gray-50 w-full h-full'>
+    <div className='flex flex-col gap-y-5 p-5 w-full h-full'>
         <h1 className='text-lg font-bold'>Dashboard <span className='text-slate-300'>/ Analytics and Reports</span></h1>
         <div className='flex gap-x-4'>
         <StatisticCard title={'Total Visitors'} previousValue={100} currentValue={136} total={42946}></StatisticCard>
         <StatisticCard title={'Paid Visitors'} previousValue={100} currentValue={126} total={7929}></StatisticCard>
         <StatisticCard title={'Total Appointments'} previousValue={100} currentValue={126} total={4199}></StatisticCard>
         <StatisticCard title={'New Patients'} previousValue={100} currentValue={126} total={1647}></StatisticCard>
+        </div>
+        <div className='grid grid-cols-[2fr_1fr] gap-x-5'>
+        <Cashflow data={{
+          graphData:[
+          {date:new Date('2025-01-01').getTime(),Income:250000},
+          {date:new Date('2025-02-01').getTime(),Income:180000},
+          {date:new Date('2025-03-01').getTime(),Income:235000},
+          {date:new Date('2025-04-01').getTime(),Income:419000},
+          {date:new Date('2025-05-01').getTime(),Income:370000},
+          {date:new Date('2025-06-01').getTime(),Income:210000},
+          {date:new Date('2025-07-01').getTime(),Income:165000},
+          {date:new Date('2025-08-01').getTime(),Income:190000},
+          {date:new Date('2025-09-01').getTime(),Income:295000},
+          {date:new Date('2025-10-01').getTime(),Income:320000},
+          {date:new Date('2025-11-01').getTime(),Income:230000},
+          {date:new Date('2025-12-01').getTime(),Income:210000},
+          ],
+          totalCashflow:4410840
+        }}></Cashflow>
+        <Expenses></Expenses>
         </div>
     </div>
   )
---
+++ src/components/Expenses.jsx
@@ -0,0 +1,81 @@
+import React, { useRef, useState } from 'react'
+import randomColor from 'randomcolor';
+
+const Expenses = () => {
+
+    const [expensesData,setExpensesData]=useState({
+          expenses:[
+            {expenseName:'Rental Cost',amount:'888240'},
+            {expenseName:'Wages',amount:'619000'},
+            {expenseName:'Supplies',amount:'467000'}
+          ],
+          totalExpenses:1822240
+    });
+
+    const expenseRef=useRef(null);
+
+    const renderExpenses=()=>{
+        const expenses=expensesData.expenses.sort((a,b)=>b.amount-a.amount).map((expense)=>{
+            let borderColor;
+            if(expense.expenseName==='Rental Cost'){
+                borderColor='#7597a5'
+            }
+            else if(expense.expenseName==='Wages'){
+                borderColor='#f0e2c5'
+            }
+            else if(expense.expenseName==='Supplies'){
+                borderColor='#aaaaf2'
+            } 
+            return {
+                expenseName:expense.expenseName,
+                amount:expense.amount,
+                borderColor:borderColor
+            };
+        })
+        
+        return (
+            <div className='flex flex-col w-full items-center gap-y-5'>
+                <div style={{borderColor:expenses[0].borderColor}} className='h-72 w-72 border-4 rounded-full p-4 flex items-center justify-center'>
+                    <div style={{borderColor:expenses[1].borderColor}} className='h-64 w-64 border-4 rounded-full p-4 flex items-center justify-center'>
+                        <div style={{borderColor:expenses[2].borderColor}} className='h-56 w-56 border-4 rounded-full p-4 flex flex-col gap-y-3 items-center justify-center font-bold'>
+                            <p>Total</p>
+                            <p className='text-2xl'>₹{expensesData.totalExpenses}</p>
+                        </div>
+                    </div>
+                </div>
+                <div className='flex justify-between w-full'>
+                    {expenses.map((expense)=>(
+                    <div className='flex flex-col gap-y-2 items-center'>
+                    <div className='flex gap-x-2'>
+                    <span style={{backgroundColor:expense.borderColor}} className='h-4 w-4 rounded-full'></span>
+                    <span className='text-xs'>{expense.expenseName}</span>
+                    </div>
+
+                    <p>₹{expense.amount}</p>
+                    </div>
+                    ))}
+                </div>
+            </div>
+            
+        )
+    }
+
+    renderExpenses();
+  return (
+    <div className='flex flex-col gap-y-20 items-center p-5 bg-white w-full rounded-lg'>
+        <div className='flex w-full justify-between items-center'>
+            <h1 className='font-bold'>Expenses</h1>
+            <div className='flex items-baseline-last gap-x-3 bg-white px-4 py-2 rounded-md border border-gray-500'>
+            <p className='text-xs text-black font-bold'>Last 12 Months</p>
+            <svg className='bi bi-chevron-dowm' xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708"/>
+            </svg>
+        </div>
+        </div>
+        
+        {renderExpenses()}
+    </div>
+  )
+}
+
+export default Expenses

ba8583e1e98885691fbc48094a25fa3f594412b4
n-muhammad-taufiq <n.muhammad.taufiq@outlook.com>
2025-09-25 13:02:03 +0530

    fix:import Dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import ProtectedLayout from "./components/ProtectedLayout"
 import Sidebar from "./components/Sidebar"
 import { Routes,Route } from "react-router-dom"
+import Dashboard from "./components/Dashboard"
 
 
 function App() {
